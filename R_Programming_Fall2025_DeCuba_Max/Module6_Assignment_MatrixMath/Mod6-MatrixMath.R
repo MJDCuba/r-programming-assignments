2226515ec7cac4655607a141c1cdfa79cedbb65b
MJDCuba <155407919+MJDCuba@users.noreply.github.com>
2025-10-02 21:44:26 -0400

    Mod 6 R file added to repo

---
+++ R_Programming_Fall2025_DeCuba_Max/Module6_Assignment_MatrixMath/Mod6-MatrixMath.R
@@ -0,0 +1,24 @@
+A <- matrix(c(2, 0, 1, 3), ncol = 2)
+B <- matrix(c(5, 2, 4, -1), ncol = 2)
+
+A
+B
+
+A + B
+A - B
+
+D <- diag(c(4, 1, 2, 3))
+
+D
+
+E <- matrix(0, nrow = 5, ncol = 5)
+
+E[1, ] <- 1
+
+E[, 1] <- 2
+
+E
+
+diag(E) <- 3
+
+E
